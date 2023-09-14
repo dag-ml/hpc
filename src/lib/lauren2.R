b06c7945a7461207a822d6b0a0668fbe789424df
bdogetlauncher <bdogetlauncher@gmail.com>
2023-09-14 13:31:16 -0700

    Fixed Duplicate Columns in Data

---
+++ src/lib/lauren2.R
@@ -0,0 +1,6 @@
+laur <- paste0(DATA_PATH, "lauren/drop_38b_chr_col_assoc_500.csv")
+laurenD <- read.csv(laur, header = TRUE, stringsAsFactors = FALSE)
+
+dsub_set <- laurenD
+title <- "OSDR-366 (Lauren)- All"
+
